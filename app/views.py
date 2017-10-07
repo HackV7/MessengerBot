d9cd347c45224943439414e6bf59d56b21624b0e
shivam kohli <kohlishivam5522@gmail.com>
2017-10-07 20:21:34 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -18,10 +18,19 @@ PAGE_ACCESS_TOKEN = 'EAABuZBXsi6owBAI8wPGGASZA0tKJZC9eZAU3RKwaSiuiV5YNHVZCfggOHf
 
 
 def post_facebook_message(fbid,message_text):
-	post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
-	response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":message_text}})
-	status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
-	print (status.json())
+    
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
+
+    if message_text == 'boarding_pass_template':
+        response_msg = boarding_pass_template(fbid)
+
+    else:
+        response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":message_text}})
+
+    requests.post(post_message_url, 
+                    headers={"Content-Type": "application/json"},
+                    data=response_msg)
+
 
 
 class MyChatBotView(generic.View):
@@ -43,9 +52,12 @@ class MyChatBotView(generic.View):
 			for message in entry['messaging']:
 				print (message)
 				try:
-					sender_id = message['sender']['id']
-					message_text = message['message']['text']
-					post_facebook_message(sender_id,message_text) 
+					if message_text == 'bpass':
+						post_facebook_message(sender_id,'boarding_pass_template')
+					else:
+						sender_id = message['sender']['id']
+						message_text = message['message']['text']
+						post_facebook_message(sender_id,message_text) 
 				except Exception as e:
 					print (e)
 					pass
@@ -54,6 +66,89 @@ class MyChatBotView(generic.View):
 
 def index(request):
 	return HttpResponse('Hello world')
+
+
+def boarding_pass_template(fbid):
+    
+    response_object = {
+						  "recipient": {
+						    "id": fbid
+						  },
+						  "message": {
+						    "attachment": {
+						      "type": "template",
+						      "payload": {
+						        "template_type": "airline_boardingpass",
+						        "intro_message": "You are checked in.",
+						        "locale": "en_US",
+						        "boarding_pass": [
+						          {
+						            "passenger_name": "SMITH\/NICOLAS",
+						            "pnr_number": "CG4X7U",
+						            "seat": "74J",            
+						            "logo_image_url": "https:\/\/www.example.com\/en\/logo.png",
+						            "header_image_url": "https:\/\/www.example.com\/en\/fb\/header.png",
+						            "qr_code": "M1SMITH\/NICOLAS  CG4X7U nawouehgawgnapwi3jfa0wfh",
+						            "above_bar_code_image_url": "https:\/\/www.example.com\/en\/PLAT.png",
+						            "auxiliary_fields": [
+						              {
+						                "label": "Terminal",
+						                "value": "T1"
+						              },
+						              {
+						                "label": "Departure",
+						                "value": "30OCT 19:05"
+						              }
+						            ],
+						            "secondary_fields": [
+						              {
+						                "label": "Boarding",
+						                "value": "18:30"
+						              },
+						              {
+						                "label": "Gate",
+						                "value": "D57"
+						              },
+						              {
+						                "label": "Seat",
+						                "value": "74J"
+						              },
+						              {
+						                "label": "Sec.Nr.",
+						                "value": "003"
+						              }
+						            ],
+						            "flight_info": {
+						              "flight_number": "KL0642",
+						              "departure_airport": {
+						                "airport_code": "JFK",
+						                "city": "New York",
+						                "terminal": "T1",
+						                "gate": "D57"
+						              },
+						              "arrival_airport": {
+						                "airport_code": "AMS",
+						                "city": "Amsterdam"
+						              },
+						              "flight_schedule": {
+						                "departure_time": "2016-01-02T19:05",
+						                "arrival_time": "2016-01-05T17:30"
+						              }
+						            }
+						          }
+						        ]
+						      }
+						    }
+						  }
+						}
+
+    return json.dumps(response_object)
+
+
+
+
+
+
 
 
 
